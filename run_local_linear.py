609bc8f394e74405f775c827fdee9349ae4af670
keptsecret <sorchon@gmail.com>
2022-08-17 05:36:24 -0400

    linearization fitting params

--- run_local_linear.py
+++ run_local_linear.py
@@ -3,18 +3,18 @@ import torch as th
 device = 'cuda' if th.cuda.is_available() else 'cpu'
 
 print("Setting up tensors")
-delta_x : th.Tensor = th.load("brownie_pca_1e-1_s0.pt")
-true_y : th.Tensor = th.load("brownie_true_output.pt")
-perturb_y : th.Tensor = th.load("brownie_pca_outputs.pt")
-delta_y : th.Tensor = perturb_y - true_y
+delta_x : th.Tensor = th.load("airplane_pca.pt")
+true_y : th.Tensor = th.load("airplane_true_output.pt")
+perturb_y : th.Tensor = th.load("airplane_pca_outputs.pt")
+delta_y : th.Tensor = perturb_y - th.flatten(true_y)
 
-A = th.rand([3*64*64, 512*128], requires_grad=True, device=device)
+A = th.rand([3*32*32, 512*128], requires_grad=True, device=device)
 criterion = th.nn.MSELoss()
 optimizer = th.optim.Adam([A], lr=1e-2)
-scheduler = th.optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=0.1)
+scheduler = th.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
 
 print("Starting fitting")
-EPOCHS = 30
+EPOCHS = 40
 for epoch in range(EPOCHS):
     running_loss = 0
     max = 0
@@ -43,4 +43,4 @@ for epoch in range(EPOCHS):
     scheduler.step()
 
 print("Finished fitting, saving matrix...")
-th.save(A, "brownie_A.pt")
+th.save(A, "airplane_A.pt")
