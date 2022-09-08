614b02663a808f13e794545a9d24365c594c4b46
keptsecret <sorchon@gmail.com>
2022-09-08 09:57:22 -0400

    adjusted model and training params

--- per_instance_nn/train_linearizer.py
+++ per_instance_nn/train_linearizer.py
@@ -6,22 +6,23 @@ from pi_features_model import PerInstanceLinearizer
 # device = 'cuda' if th.cuda.is_available() else 'cpu'
 # device = 'cpu'
 # device = 'cuda:0'
-# dtype = th.float16
+dtype = th.float32
 
 model = PerInstanceLinearizer()
 model.load_encoder_weights("pi_encoder_weights_x.pt")
+model = model.to(dtype=dtype)
 
-EPOCHS = 200
-learning_rate = 1e-4
+EPOCHS = 50
+learning_rate = 5e-4
 optimizer = th.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-8)
-scheduler = th.optim.lr_scheduler.StepLR(optimizer, 10, gamma=0.2)
+scheduler = th.optim.lr_scheduler.StepLR(optimizer, 10, gamma=0.5)
 
 th.autograd.set_detect_anomaly(True)
 
-x = th.load("../brownie_true_input_[xf_out].pt")
+x = th.load("../brownie_true_input_[xf_out].pt").to(dtype=dtype)
 x = x[0]  # remove classifier-free guidance stack
 
-y = th.load("../brownie_variations_true_output.pt").to("cuda:1")
+y = th.load("../brownie_variations_true_output.pt").to("cuda:1", dtype=dtype)
 y = y.flatten()
 
 def criterion(A, dxs, dys):
@@ -29,9 +30,10 @@ def criterion(A, dxs, dys):
 
     value = 0.0
     for dx, dy in zip(dxs, dys):
-        pred_dy = th.matmul(A, dx)
+        enc_dx = model.encode(dx) #.to('cuda:0'))
+        pred_dy = th.matmul(A, enc_dx) #.to('cpu'))
         value += loss_fn(pred_dy, dy)
-    
+
     return value / dxs.shape[0]
 
 for epoch in range(EPOCHS):
@@ -40,15 +42,15 @@ for epoch in range(EPOCHS):
     for i in range(180):
         optimizer.zero_grad()
 
-        dx = th.load(f"../brownie_variations/brownie_variation{i}_pca.pt").to("cuda:0")
+        dx = th.load(f"../brownie_variations/brownie_variation{i}_pca.pt").to('cuda:0', dtype=dtype)
         dx = dx.T[:, None, :].repeat(1, 512, 1)
 
         A_mat = model(x)
 
-        new_y = th.load(f"../brownie_variations_outputs/brownie_variation{i}_pca_output.pt").to("cuda:1")
+        new_y = th.load(f"../brownie_variations_outputs/brownie_variation{i}_pca_output.pt").to('cuda:1', dtype=dtype)
         dy = new_y - y
 
-        loss = criterion(A_mat, dx.to('cpu'), dy.to('cpu'))
+        loss = criterion(A_mat.to('cuda:0'), dx.to('cuda:0'), dy.to('cuda:0'))
         loss.backward()
 
         optimizer.step()
