67ff03891ce99580f4b87fc3cc63d846fe4c9f8d
keptsecret <sorchon@gmail.com>
2022-09-18 10:24:42 -0400

    trained new encoder

--- per_instance_nn/train_autoencoder.py
+++ per_instance_nn/train_autoencoder.py
@@ -6,15 +6,15 @@ from pi_features_model import AE
 device = 'cuda' if th.cuda.is_available() else 'cpu'
 # device = 'cpu'
 # device = 'cuda:0'
-dtype = th.float16
+dtype = th.float32
 
 autoencoder = AE()
 autoencoder = autoencoder.to(device, dtype=dtype)
 
-EPOCHS = 5
+EPOCHS = 20
 learning_rate = 1e-4
 optimizer = th.optim.Adam(autoencoder.parameters(), lr=learning_rate, weight_decay=1e-6)
-# scheduler = th.optim.lr_scheduler.MultiStepLR(optimizer, [10], gamma=0.1) # doesn't work
+scheduler = th.optim.lr_scheduler.MultiStepLR(optimizer, [10], gamma=0.1) # doesn't work
 criterion = nn.MSELoss()
 
 th.autograd.set_detect_anomaly(True)
@@ -45,9 +45,9 @@ for epoch in range(EPOCHS):
         optimizer.step()
         running_loss += loss.item()
         print(f"\rIter: {i} - loss: {loss.item():.5f}", end='')
-    # scheduler.step()
+    scheduler.step()
 
     print(f'\rEpoch: {epoch+1} - loss: {running_loss / 180:.5f}')
 
-autoencoder.save_encoder_weights("pi_encoder_weights_dx.pt")
+autoencoder.save_encoder_weights("pi_betterencoder_weights_dx.pt")
 print('Training complete')
